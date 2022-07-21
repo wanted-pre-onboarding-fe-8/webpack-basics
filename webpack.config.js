23cc048bb5a866e1f77e2c1631e2347e0c6e55b6
Aum Il-Kyeong <sunaerocket@gmail.com>
2022-07-21 18:23:43 +0900

    chore: 웹팩 output 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,8 @@ module.exports = {
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].bundle.js',
+  },
 };
