22af1781b4f4a519a36514ab8ca6f6edd0d9f8ee
Aum Il-Kyeong <sunaerocket@gmail.com>
2022-07-21 18:52:46 +0900

    chore: index.js 파일에 DOM 조작 로직 추가

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('내가 보이면 웹팩 설정 성공!');
+const heading = document.createElement('h1');
+heading.textContent = '내가 보이면 성공!';
+
+const app = document.querySelector('#root');
+app.append(heading);
