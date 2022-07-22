d0a12b3379aa9b6dfcb5d1e612e546d7363cf749
Aum Il-Kyeong <sunaerocket@gmail.com>
2022-07-22 16:06:55 +0900

    chore: 배포용 웹팩 설정에서 불필요한 임포트 제거

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,5 +1,3 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { merge } = require('webpack-merge');
 
 const paths = require('./paths');
